7fcbd3527aa4bf6862d7ee8668a7b44e0474efd4
Joranome <joranome@gmail.com>
2022-02-16 17:18:51 +0000

    Finalizando 00Saludo

--- assignments/00HelloWorld/src/exercise.py
+++ assignments/00HelloWorld/src/exercise.py
@@ -1,6 +1,9 @@
 def main():
-    #escribe tu código abajo de esta línea
-    pass
+    nombre=input('Dame tu nombre: ')
+    apellido_paterno=input('Dame tu apellido paterno: ')
+    apellido_materno=input('Dame tu apellido materno: ')
+    nombre_completo=nombre+' '+apellido_paterno+' '+apellido_materno
+    print(nombre_completo)
 
 if __name__=='__main__':
     main()
